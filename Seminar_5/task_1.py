0663913d8edab6a8a7ddecbea62da83622ed29ab
Nikita <nikitapohod@icloud.com>
2022-10-14 12:20:58 +0300

    Task 1 solved

---
+++ Seminar_5/task_1.py
@@ -0,0 +1,9 @@
+# Напишите программу, удаляющую из текста все слова, содержащие "абв".
+
+def delete_words(origin_string: str, del_factor: str):
+    return " ".join(list(filter(lambda el: del_factor not in el, origin_string.lower().split())))
+
+
+test_string = "аБВ -влвы- сао б вба оабв абвввв"
+res = delete_words(test_string, "абв")
+print(res)
